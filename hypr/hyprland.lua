af54d5983f4ddf27c87c10242e5a01e02d990326
RoundUpSSBM <thomasvanoosterbosch@gmail.com>
2026-05-19 18:36:47 +0200

    fix: proper 5% brightness intervals

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -137,6 +137,7 @@ hl.permission({ binary = "/usr/bin/grim", type = "screencopy", mode = "allow" })
 hl.permission({ binary = "/usr/lib/xdg-desktop-portal-hyprland", type = "screencopy", mode = "allow"})
 hl.permission({ binary = "/home/thomas/development/Hyprfoci/hyprfoci.so", type = "plugin", mode = "allow" })
 hl.permission({ binary = "/var/cache/hyprpm/thomas/Hyprfoci/hyprfoci.so", type = "plugin", mode = "allow" })
+hl.permission({ binary = "/var/cache/hyprpm/thomas/hyprland-plugins/csgo-vulkan-fix.so", type = "plugin", mode = "allow" })
 
 ---------------------
 --- LOOK AND FEEL ---
@@ -374,8 +375,8 @@ hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_
 hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
 hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
 hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 20000+"),                  { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 20000-"),                  { locked = true, repeating = true })
 
 -- Requires playerctl
 hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
